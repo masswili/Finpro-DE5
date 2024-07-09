66d93f80a58d4d57a1b2883881c4456c82a4bc65
WildanIlham <93912056+masswili@users.noreply.github.com>
2024-07-09 12:53:40 +0700

    New code added

--- batch_processing.py
+++ batch_processing.py
@@ -1,7 +1,12 @@
 from pyspark.sql import SparkSession
+import time
 
 # Inisialisasi Spark
-spark = SparkSession.builder.appName("RussiaLossesBatchProcessing").getOrCreate()
+spark = SparkSession.builder \
+    .appName("RussiaLossesBatchProcessing") \
+    .config("spark.jars", "D:\Finpro-Data Engineer\Finpro-DE5\\postgresql-42.7.3.jar") \
+    .config("spark.local.dir", "D:\Finpro-Data Engineer\Finpro-DE5\spark-temp") \
+    .getOrCreate()
 
 # Memuat data
 data_path = 'transformed_data.csv'
@@ -17,4 +22,9 @@ df_spark_cleaned.write \
     .option("dbtable", "russia_losses_equipment_cleaned") \
     .option("user", "postgres") \
     .option("password", "admin123") \
+    .option("driver", "org.postgresql.Driver") \
     .save()
+
+# Stop Spark session
+time.sleep(5)
+spark.stop()
