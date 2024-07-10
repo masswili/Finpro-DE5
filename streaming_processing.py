e8ced311d1fbd6f105c265d7d26e2849506b6f2b
WildanIlham <93912056+masswili@users.noreply.github.com>
2024-07-10 23:23:29 +0700

    Update doc-delivery

--- streaming_processing.py
+++ streaming_processing.py
@@ -3,16 +3,19 @@ from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StructField, StringType, DateType, IntegerType
 
 # Inisialisasi Spark
-spark = SparkSession.builder.appName("RussiaLossesStreamingProcessing").getOrCreate()
+spark = SparkSession.builder \
+    .appName("RussiaLossesStreamingProcessing") \
+    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,org.postgresql:postgresql:42.7.3") \
+    .getOrCreate()
 
-# Definisi skema untuk data streaming
+# Define skema untuk streaming data
 schema = StructType([
     StructField("date", DateType(), True),
     StructField("equipment", StringType(), True),
     StructField("losses", IntegerType(), True)
 ])
 
-# Membaca stream dari Kafka
+# Read stream from Kafka
 df_stream = spark \
     .readStream \
     .format("kafka") \
@@ -20,17 +23,24 @@ df_stream = spark \
     .option("subscribe", "russia_losses_equipment") \
     .load()
 
-# Transformasi data
+# Transform data
 df_stream = df_stream.selectExpr("CAST(value AS STRING)") \
     .select(from_json(col("value"), schema).alias("data")) \
     .select("data.*")
 
-# Menyimpan data ke PostgreSQL
-df_stream.writeStream \
+# Save data to PostgreSQL with checkpointing
+checkpoint_location = "D:\Finpro-Data Engineer\Finpro-DE5\checkpoint\dir"
+
+
+query = df_stream.writeStream \
     .format("jdbc") \
     .option("url", "jdbc:postgresql://localhost:5432/postgres") \
     .option("dbtable", "russia_losses_equipment_streaming") \
     .option("user", "postgres") \
     .option("password", "admin123") \
-    .start() \
-    .awaitTermination()
+    .option("driver", "org.postgresql.Driver") \
+    .option("checkpointLocation", checkpoint_location) \
+    .outputMode("append") \
+    .start()
+
+query.awaitTermination()
