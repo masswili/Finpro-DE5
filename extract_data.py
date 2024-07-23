d0dceeeef1ae69b05c518555fd2bb05c23232fd1
WildanIlham <93912056+masswili@users.noreply.github.com>
2024-07-23 13:30:38 +0700

    Remidial

--- extract_data.py
+++ extract_data.py
@@ -10,11 +10,11 @@ def extract_data():
         # Ekstraksi data dari file CSV
         df_csv = pd.read_csv('D:\Finpro-Data Engineer\Finpro-DE5\Data\russia_losses_equipment_correction.csv')
         # Ekstraksi data dari API
-        response = requests.get('https://api.example.com/data')
-        data_api = response.json()
-        df_api = pd.DataFrame(data_api)
+        # response = requests.get('https://api.example.com/data')
+        # data_api = response.json()
+        # df_api = pd.DataFrame(data_api)
         # Gabungkan data
-        df_combined = pd.merge(df_csv, df_api, on='key_column')
+        df_combined = pd.merge(df_csv, on='key_column')
         df_combined.to_csv('D:\Finpro-Data Engineer\Finpro-DE5\extracted_data.csv', index=False)
         logging.info("Ekstraksi data selesai.")
     except Exception as e:
